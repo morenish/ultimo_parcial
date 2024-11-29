53254d740ea1dc25af07a60268ded5f49b2eb562
morenish <samuel.redonda@ulv.edu.mx>
2024-11-29 16:04:29 -0600

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-console.log("Bienvenido al programa de saludos.");
-
-function saludar(nombre){
-    return `Hola, ${nombre}! ¿Como estas?`;
-}
-
-const nombreUsuario = "Estudiante";
-console.log(saludar(nombreUsuario));
-
